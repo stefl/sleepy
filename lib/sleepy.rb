78c4d0fb1b677e437d8568573509bbe454246c9f
steflewandowski <stef@stef.io>
2010-09-17 22:06:07 +0100

    only cache if the response is 200

--- lib/sleepy.rb
+++ lib/sleepy.rb
@@ -29,6 +29,7 @@ module Weary
     def perform_sleepily(timeout=60*60*1000, &block)
       @on_complete = block if block_given?
       if HAS_MEMCACHE
+        timeout = ENV["SLEEPY_TIMEOUT"] if defined?(ENV["SLEEPY_TIMEOUT"])
         response = perform_sleepily!(timeout)
       else
         response = perform
@@ -37,13 +38,11 @@ module Weary
     end
     
     # Redefine the perform method
-    def perform_sleepily!(timeout=60*60*1000, &block)
+    def perform_sleepily!(timeout, &block)
       @on_complete = block if block_given?
       Thread.new {
-        STDERR.puts "Before send: #{uri}"
         before_send.call(self) if before_send
         
-        STDERR.puts "try nap"
         nap = sleepy.get("#{round_time(Time.new.to_i, timeout)}:#{uri}") rescue nil
         
         unless nap.blank?
@@ -60,8 +59,10 @@ module Weary
               on_complete.call(response) if on_complete
               response
             end
-            sleepy.set("#{round_time(Time.new.to_i, timeout)}:#{uri}", response)
-            sleepy.set("0:#{uri}", response)
+            if response.code && response.code == 200
+              sleepy.set("#{round_time(Time.new.to_i, timeout)}:#{uri}", response)
+              sleepy.set("0:#{uri}", response)
+            end
           rescue
             sleepy.get("0:#{uri}") rescue nil
           end
